e41f5b4d34416f60ea295431e2ea6399f90bcf31
JeronimoMendes <jeronimo.m.mendes@gmail.com>
2021-01-09 21:59:17 +0000

    Create CommonBalance class

--- common_balance/models.py
+++ common_balance/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class CommonBalance(models.Model):
+
+    user1 = models.OneToOneField(User1, on_delete=models.CASCADE)
+    user2 = models.OneToOneField(User2, on_delete=models.CASCADE)
