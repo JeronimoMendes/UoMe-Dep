f4dd048527f9587b208b8551fbc4c294f57b8edf
JeronimoMendes <jeronimo.m.mendes@gmail.com>
2021-01-17 16:17:47 +0000

    decimal correction

--- common_balance/views.py
+++ common_balance/views.py
@@ -49,8 +49,8 @@ def debt_dashboard(request):
 
     if request.method == "GET":
         user = request.user
-        common_accs = [(i, i.other_user(user), i.how_much_debt(user)) for i in CommonAccount.objects.filter(user1=user)]
-        common_accs += [(i, i.other_user(user), i.how_much_debt(user)) for i in CommonAccount.objects.filter(user2=user)]
+        common_accs = [(i, i.other_user(user), i.how_much_debt(user)/100) for i in CommonAccount.objects.filter(user1=user)]
+        common_accs += [(i, i.other_user(user), i.how_much_debt(user)/100) for i in CommonAccount.objects.filter(user2=user)]
         context = {"common_accs":common_accs,}
         return render(request, "dashboard/dashboard.html", context)
  
