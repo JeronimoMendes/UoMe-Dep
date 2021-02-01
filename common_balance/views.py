f98c5911da4c3b850b5402b43586252adf1e3443
JeronimoMendes <jeronimo.m.mendes@gmail.com>
2021-02-01 03:24:52 +0000

    Add Logging system to common_account

--- common_balance/views.py
+++ common_balance/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from .models import CommonAccount
-from users.models import Profile
+from .models import CommonAccount, Log
+from users.models import Profile, FriendRequest
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
-from users.models import FriendRequest
 # Create your views here.
 def debt_dashboard(request):   
     def get_context():
@@ -40,13 +39,15 @@ def common_acc(request):
         account = CommonAccount.objects.get(id=query)
         user = request.user
         nr_friend_requests = len(FriendRequest.objects.filter(to_user=user))
+        logs = Log.objects.filter(common_account=account)
 
         context = {
             "other_user": account.other_user(user.username),
             "debt": account.how_much_debt(user.username)/100,
             "owed": account.how_much_owes(user.username)/100,
             "acc": account,
-            'nr_friend_requests': nr_friend_requests
+            'nr_friend_requests': nr_friend_requests,
+            "logs": logs
         }
 
         return context
@@ -62,11 +63,16 @@ def common_acc(request):
         print(request.GET.get('value_inc_owed'))
         inc_debt = float(request.GET.get('value_inc_debt'))
         inc_owed = float(request.GET.get('value_inc_owed'))
+        reason = request.GET.get('reason')
 
         account_id = request.GET.get('account')
         account = CommonAccount.objects.get(id=account_id)
 
         account.increase_debt(request.user, int(inc_debt*100))
         account.decrease_debt(request.user, int(inc_owed*100))
+
+        Log.objects.create(by_user=request.user, common_account=account, reason=reason, inc_debt=inc_debt, inc_owed=inc_owed)
+
+
         
         return redirect("/common_account/?q={}".format(account_id))
